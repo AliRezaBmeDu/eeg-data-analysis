e071083fe357aaf0aba4bf8f112f869af2811256
AliRezaBmeDu <ali.reza.buet@gmail.com>
2024-01-22 22:20:33 +0600

    Determine the number of channels

--- eeg-data-analysis/start.py
+++ eeg-data-analysis/start.py
@@ -20,3 +20,15 @@ df_eeg = pd.read_parquet(BASE_PATH + 'train_eegs/1000913311.parquet')
 df_eeg.head()
 
 
+# Determine the number of channels
+# Assuming each row is a time point and each column is a channel
+n_channels = df_eeg.shape[1]
+n_channels
+
+df = pd.read_csv(BASE_PATH + 'train.csv')
+TARGETS = df.columns[-6:]
+print('Train shape:', df.shape )
+print('Targets', list(TARGETS))
+df.head()
+
+
